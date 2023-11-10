52069b66b8a0eb81b4c2fa0e1a8e6ac0bd5cce28
baoat <baoat191201@gmail.com>
2023-11-10 17:45:39 +0700

    Hoàn thành

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import style from "./App.scss";
 import classNames from "classnames/bind";
 
 import logoNCC from "./acsets/img/logoNCC 1.png";
-// import cssIcon from "./acsets/img/cssIcon 1.png";
-// import htmlIcon from "./acsets/img/htmlIcon 1.png";
-// import urlIcon from "./acsets/img/urlIcon 1.png";
+import cssIcon from "./acsets/img/cssIcon 1.png";
+import htmlIcon from "./acsets/img/htmlIcon 1.png";
+import urlIcon from "./acsets/img/urlIcon 1.png";
 
 const cx = classNames.bind(style);
 
 function App() {
   return (
-    <div className={cx("wrapper grid ")}>
-      <div className={cx("row no-gutters wrapper-header")}>
+    <div className={cx("grid ")}>
+      <div className={cx("row no-gutters wrapper")}>
         <header className={cx("header col l-1 m-12 c-12")}>
           <label for="nav__show" className={cx("btn__nav")}>
             <i class="fa-solid fa-bars"></i>
@@ -26,7 +26,9 @@ function App() {
 
           <ul className={cx("header-list")}>
             <li className={cx("header-list-item")}>
-              <div className={cx("header-list-item__item active")}>Home</div>
+              <div className={cx("header-list-item__item active")}>
+                <label for="nav__show">Home</label>
+              </div>
             </li>
             <li className={cx("header-list-item")}>
               <div className={cx("header-list-item__item")}>Services</div>
@@ -46,8 +48,8 @@ function App() {
           <div className={cx("content")}>
             <div className={cx("row")}>
               <div className={cx("col l-10 l-o-1")}>
-                <div className={cx("row")}>
-                  <div className={cx("col l-2 l-o-5 m-2 m-o-5 c-2 c-o-5")}>
+                <div className={cx("row", "content-logo")}>
+                  <div className={cx("col l-4 l-o-4 m-4 m-o-4 c-4 c-o-4")}>
                     <div className={cx("logo")}>
                       <img src={logoNCC} alt="" width={"100%"} />
                     </div>
@@ -84,34 +86,90 @@ function App() {
                     </div>
                   </div>
                 </div>
-                <div className={cx(" row course")}>
+                <div className={cx("row course")}>
                   <div className={cx("course-item")}>
-                    <div className={cx("course-item__logo")}></div>
-                    <div className={cx("course-item__content")}></div>
+                    <span className={cx("course-item__title")}>
+                      Lorem ipsum dolor sit amet
+                    </span>
+                    <div className={cx("row no-gutters course-item__logo")}>
+                      <div className={cx("col l-3 m-3 c-12")}>
+                        <img src={cssIcon} alt="" width={"100%"} />
+                      </div>
+                      <div className={cx("col l-9 m-9 c-12", "content")}>
+                        <span>
+                          Lorem ipsum dolor sit amet, consectetur adipiscing
+                          elit. Sed non dui sodales, faucibus libero ut, posuere
+                          felis. Donec imperdiet suscipit accumsan. Aenean
+                          consequat condimentum velit ut tempor. Nam porta massa
+                          in metus bibendum congue. Pellentesque ultrices liquam
+                          egestas nunc at{" "}
+                        </span>
+                      </div>
+                    </div>
+                    <div className={cx("course-item__content")}>
+                      <span>
+                        ullamcorper ultricies. Donec feugiat velit nulla, vel
+                        sodales est ullamcorper id. Aenean consequat condimentum
+                        velit ut tempor. Nam porta massa in metus bibendum
+                        congue. Pellentesque ultrices vestibulum mattis.
+                      </span>
+                    </div>
                   </div>
                   <div className={cx("course-item")}>
-                    <div className={cx("course-item__logo")}></div>
-                    <div className={cx("course-item__content")}></div>
+                    <span className={cx("course-item__title")}>
+                      Lorem ipsum dolor sit amet
+                    </span>
+                    <div className={cx("row no-gutters course-item__logo")}>
+                      <div className={cx("col l-3 m-3 c-12")}>
+                        <img src={htmlIcon} alt="" width={"100%"} />
+                      </div>
+                      <div className={cx("col l-9 m-9 c-12", "content")}>
+                        <span>
+                          Lorem ipsum dolor sit amet, consectetur adipiscing
+                          elit. Sed non dui sodales, faucibus libero ut, posuere
+                          felis. Donec imperdiet suscipit accumsan. Aenean
+                          consequat condimentum velit ut tempor. Nam porta massa
+                          in metus bibendum congue. Pellentesque ultrices liquam
+                          egestas nunc at{" "}
+                        </span>
+                      </div>
+                    </div>
+                    <div className={cx("course-item__content")}>
+                      <span>
+                        ullamcorper ultricies. Donec feugiat velit nulla, vel
+                        sodales est ullamcorper id. Aenean consequat condimentum
+                        velit ut tempor. Nam porta massa in metus bibendum
+                        congue. Pellentesque ultrices vestibulum mattis.
+                      </span>
+                    </div>
                   </div>
                   <div className={cx("course-item")}>
-                    <div className={cx("course-item__logo")}></div>
-                    <div className={cx("course-item__content")}></div>
-                  </div>
-                  <div className={cx("course-item")}>
-                    <div className={cx("course-item__logo")}></div>
-                    <div className={cx("course-item__content")}></div>
-                  </div>
-                  <div className={cx("course-item")}>
-                    <div className={cx("course-item__logo")}></div>
-                    <div className={cx("course-item__content")}></div>
-                  </div>
-                  <div className={cx("course-item")}>
-                    <div className={cx("course-item__logo")}></div>
-                    <div className={cx("course-item__content")}></div>
-                  </div>
-                  <div className={cx("pandabar")}>
-                    <div className={cx("course-item__logo")}></div>
-                    <div className={cx("course-item__content")}></div>
+                    <span className={cx("course-item__title")}>
+                      Lorem ipsum dolor sit amet
+                    </span>
+                    <div className={cx("row no-gutters course-item__logo")}>
+                      <div className={cx("col l-3 m-3 c-12")}>
+                        <img src={urlIcon} alt="" width={"100%"} />
+                      </div>
+                      <div className={cx("col l-9 m-9 c-12", "content")}>
+                        <span>
+                          Lorem ipsum dolor sit amet, consectetur adipiscing
+                          elit. Sed non dui sodales, faucibus libero ut, posuere
+                          felis. Donec imperdiet suscipit accumsan. Aenean
+                          consequat condimentum velit ut tempor. Nam porta massa
+                          in metus bibendum congue. Pellentesque ultrices liquam
+                          egestas nunc at{" "}
+                        </span>
+                      </div>
+                    </div>
+                    <div className={cx("course-item__content")}>
+                      <span>
+                        ullamcorper ultricies. Donec feugiat velit nulla, vel
+                        sodales est ullamcorper id. Aenean consequat condimentum
+                        velit ut tempor. Nam porta massa in metus bibendum
+                        congue. Pellentesque ultrices vestibulum mattis.
+                      </span>
+                    </div>
                   </div>
                 </div>
               </div>
@@ -119,9 +177,7 @@ function App() {
             <div className={cx("row")}>
               <div className={cx("col l-12 m-12 c-12")}>
                 <div className={cx("row footer ")}>
-                  <div
-                    className={cx("col l-2 l-o-0-5 m-2 m-o-0-5 c-3 c-o-0-5 ")}
-                  >
+                  <div className={cx("col l-2 l-o-1 m-2 m-o-1 c-3 c-o-1 ")}>
                     <div className={cx("footer__content")}>
                       Copyright © 2021
                     </div>
